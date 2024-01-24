2b83c24de944cb57682cffa790141d55ce22988b
sterlker <151909596+sterlker@users.noreply.github.com>
2024-01-24 18:41:43 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/buddyapps2/MainActivity.java
+++ app/src/main/java/com/example/buddyapps2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.buddyapps2;
 // COMMIT AND PUSH TRY
+// Hi
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
